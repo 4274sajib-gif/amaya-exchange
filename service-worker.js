a560eff2fa1acff1dc2fb45d4be67b80dbf25ad1
4274sajib-gif <4274sajib@gmail.com>
2026-09-02 13:13:17 +0300

    Refactor service worker to use APP_FILES array

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,22 @@
-const CACHE_NAME = "amaya-exchange-v1";
+const CACHE_NAME = 'amaya-exchange-v1';
 
-const APP_SHELL = [
-  "/amaya-exchange/",
-  "/amaya-exchange/index.html",
-  "/amaya-exchange/manifest.json"
+const APP_FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
+      .then(cache => cache.addAll(APP_FILES))
       .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
@@ -22,27 +24,15 @@ self.addEventListener("activate", event => {
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
       )
-    ).then(() => self.clients.claim())
+    )
   );
+
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-
-  if (event.request.method !== "GET") return;
-
+self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request)
-      .then(response => {
-
-        const copy = response.clone();
-
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, copy);
-        });
-
-        return response;
-      })
       .catch(() => caches.match(event.request))
   );
-
 });
